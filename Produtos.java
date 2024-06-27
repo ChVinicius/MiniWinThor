6f26e2ab0c263ece61fdd172027563b822c94807
ChVinicius <charlesviniciussousaaraujo@gmail.com>
2024-06-26 21:38:58 -0300

    Criação classe Produto

---
+++ Produtos.java
@@ -0,0 +1,25 @@
+public class Produtos {
+
+    private int codigo;
+    private String descricao;
+    private double precoDeCompra;
+    private double precoDeVenda;
+    private String status;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setPrecoDeCompra(double precoDeCompra) {
+        this.precoDeCompra = precoDeCompra;
+    }
+    public void setPrecoDeVenda(double precoDeVenda) {
+        this.precoDeVenda = precoDeVenda;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
