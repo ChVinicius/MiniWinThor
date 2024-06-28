00d8846ee177415ec79b99cc0b3d03b49aa78a60
ChVinicius <charlesviniciussousaaraujo@gmail.com>
2024-06-27 21:40:12 -0300

    Adição com metodos e Array.

--- Produtos.java
+++ Produtos.java
@@ -6,18 +6,43 @@ public class Produtos {
     private double precoDeVenda;
     private String status;
 
+
+    public int getCodigo(){
+       return codigo;
+    }
+
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
+
+    public int getDescricao(){
+        return codigo;
+    }
+
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    public double getPrecoDeCompra(){
+       return precoDeCompra;
+    }
+
     public void setPrecoDeCompra(double precoDeCompra) {
         this.precoDeCompra = precoDeCompra;
     }
+
+    public double getPrecoDeVenda(){
+       return precoDeVenda;
+    }
+
     public void setPrecoDeVenda(double precoDeVenda) {
         this.precoDeVenda = precoDeVenda;
     }
+
+    public String getStatus(){
+        return status;
+    }
+
     public void setStatus(String status) {
         this.status = status;
     }
